9ba016a95048204acd7abc0f74a4a4610a0e3875
Sharon <keglevich2@gmail.com>
2014-04-05 21:30:34 +0300

    plugin modified

---
+++ src/android/WiseNotificationService.java
@@ -0,0 +1,33 @@
+package com.example.intenttest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+
+public class WiseNotificationService extends IntentService {
+
+
+	public WiseNotificationService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public WiseNotificationService() {
+        super("WiseNotificationService");
+    }
+	
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		
+		if (intent.getStringExtra("DoIt").equals("sharon"))
+		{
+			//Handler handler = new Handler();
+			
+			Log.d("TagMe","success");
+		}
+		else
+			Log.d("TagMe","fail");	
+	}
+
+}
